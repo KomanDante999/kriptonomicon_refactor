2b510263a5c28c824691bbe2a13482bf285d0aa5
komandante999 <dreftware@yandex.ru>
2023-07-12 11:47:40 +0700

    ref 3

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const API_KEY =
-  "c5830f5e427fa14670e0d35f1fd17a70a42551bac738247d2bb81ed27a20b5ae";
+  "4969353a3c3c68dc7c604fc143f9f091fecaa8bf2cef74d4630c47d80613599a";
 
 export const loadTicker = (tickers) =>
   fetch(
-    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=&${tickers.join(
+    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tickers.join(
       ","
     )}tsyms=USD&api_key=${API_KEY}`
   ).then((response) => response.json());
